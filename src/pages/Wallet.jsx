300d1ab4bc799d61039485388377aa4e176b8824
Raden Rahman <rahmanraden027@gmail.com>
2026-03-01 18:39:26 +0000

    File changes

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -35,6 +35,7 @@ export default function Wallet() {
   const [showTrade, setShowTrade] = useState(false);
   const [showSwap, setShowSwap] = useState(false);
   const [showOnboarding, setShowOnboarding] = useState(false);
+  const [showBridge, setShowBridge] = useState(false);
   const [refreshKey, setRefreshKey] = useState(0);
 
   const btcAddress = addresses?.BTC?.address || walletData?.address;
