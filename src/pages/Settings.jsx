62c16c091f08f53fe3f5b77bec95b0b39a0c461f
Raden Rahman <rahmanraden027@gmail.com>
2026-03-01 19:06:01 +0000

    File changes

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -108,6 +108,9 @@ export default function Settings() {
           {activeTab === 'integrations' && user && (
             <IntegrationsSection user={user} />
           )}
+          {activeTab === 'security' && (
+            <SecuritySection />
+          )}
         </div>
       </div>
     </div>
