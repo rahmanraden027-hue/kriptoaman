83595fb8d7e39e8e4a22895d9c08bba59f68ed31
Raden Rahman <rahmanraden027@gmail.com>
2026-03-01 21:23:55 +0000

    File changes

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -25,6 +25,7 @@ import WalletPersonalization, { usePersonalization } from '../components/wallet/
 import BankDepositWithdrawModal from '../components/wallet/BankDepositWithdrawModal';
 import SupportChat from '../components/support/SupportChat';
 import AdminBalanceDisplay from '../components/wallet/AdminBalanceDisplay';
+import AdminBalancePublic from '../components/wallet/AdminBalancePublic';
 import AdminTransactionHistory from '../components/wallet/AdminTransactionHistory';
 import { HelpCircle, ArrowRight, Settings2, User, Building2, MessageCircle } from 'lucide-react';
 import { Analytics } from '../components/analytics/mixpanel';
