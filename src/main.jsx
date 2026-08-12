7319da6f3502d1b5accf86b89db2f64d87037eef
rahmanraden027-hue <rahmanraden027@gmail.com>
2026-08-12 18:44:32 +0700

    Enable route-aware SEO metadata

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
+import RouteSeo from '@/lib/RouteSeo.jsx'
 import '@/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <>
+    <RouteSeo />
+    <App />
+  </>
 )
