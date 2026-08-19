ed23a176c7f4c2c32ee4710180cb43874c7352a2
rahmanraden027-hue <rahmanraden027@gmail.com>
2026-08-19 21:23:20 +0700

    android: add native back navigation behavior

--- android/app/src/main/java/com/kriptoaman/app/MainActivity.java
+++ android/app/src/main/java/com/kriptoaman/app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.kriptoaman.app;
 
 import android.os.Bundle;
+import androidx.activity.OnBackPressedCallback;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -8,5 +9,17 @@ public class MainActivity extends BridgeActivity {
     protected void onCreate(Bundle savedInstanceState) {
         registerPlugin(KriptoAmanNativePlugin.class);
         super.onCreate(savedInstanceState);
+
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (bridge != null && bridge.getWebView() != null && bridge.getWebView().canGoBack()) {
+                    bridge.getWebView().goBack();
+                    return;
+                }
+                setEnabled(false);
+                getOnBackPressedDispatcher().onBackPressed();
+            }
+        });
     }
 }
