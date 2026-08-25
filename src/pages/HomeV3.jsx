68fff709879e1910c65aa0aa72a80b4e6cb00312
rahmanraden027-hue <rahmanraden027@gmail.com>
2026-08-26 00:09:52 +0700

    Tighten final Home bottom spacing on mobile

--- src/pages/HomeV3.jsx
+++ src/pages/HomeV3.jsx
@@ -4,7 +4,14 @@ import HomeInstitutionalFooter from '@/components/home/HomeInstitutionalFooter';
 
 export default function HomeV3() {
   return (
-    <div className="ka-bg min-h-screen text-white">
+    <div className="ka-home-v3 ka-bg min-h-screen text-white">
+      <style>{`
+        @media (max-width: 1023px) {
+          body:has(.ka-home-v3) .ka-global-shell > div:last-child {
+            height: 5.5rem;
+          }
+        }
+      `}</style>
       <HomeV2 />
       <div className="mx-auto -mt-24 max-w-7xl px-4 pb-1 sm:px-6 sm:pb-2 lg:px-8 lg:pb-0">
         <HomeInstitutionalFooter />
